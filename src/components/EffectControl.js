a3dacc42b527f95e8aef54c6e90bdd84c31af420
billymoroney <YOUR-EMAIL-ADDRESS>
2021-02-06 14:05:09 -0600

    effect buttons are properly updating a higher order 'synth' array that will store synth data

--- src/components/EffectControl.js
+++ src/components/EffectControl.js
@@ -5,8 +5,14 @@ export default function EffectControl(props) {
     const [enabled, setEnabled] = useState(false)
 
     const handleClick = (e) => {
-        if (!enabled) setEnabled(true)
-        else setEnabled(false)
+        if (!enabled) {
+            setEnabled(true)
+            props.add(props.name)
+        }
+        else {
+            setEnabled(false)
+            props.remove(props.name)
+        }
     }
     return (
         <div className={enabled ? 'on-effect' : 'off-effect'} onClick={handleClick}>
--- src/components/UI.js
+++ src/components/UI.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import MainOnOff from './MainOnOff'
 import Trigger from './Trigger'
 import EffectControl from './EffectControl'
 
 export default function UI() {
+
+    const [synth, setSynth] = useState([])
+
+    const effectAdd = (name) => {
+        setSynth([...synth, name])
+    }
+
+    useEffect(() => {
+        console.log(synth)
+    }, [synth])
+
+    const effectRemove = (name) => {
+        let i = synth.indexOf(name)
+        synth.splice(i, 1)
+        setSynth([...synth])
+    }
+
     return (
         <div className='flex w-3/5 m-auto flex-col space-y-12'>
             <div className='h-42 flex justify-around content-center'>
@@ -11,8 +28,8 @@ export default function UI() {
                 <MainOnOff />
             </div>
             <div className='flex justify-around'>
-                <EffectControl name={'reverb'} />
-                <EffectControl name={'filter'} />
+                <EffectControl name={'reverb'} add={effectAdd} remove={effectRemove} />
+                <EffectControl name={'filter'} add={effectAdd} remove={effectRemove} />
             </div>
         </div>
     )
