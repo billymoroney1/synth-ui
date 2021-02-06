819761b7087cf535588509e0eef59cbbc9617444
billymoroney <YOUR-EMAIL-ADDRESS>
2021-02-06 12:31:25 -0600

    made a couple buttons that will use to turn effects on/off

---
+++ src/components/EffectControl.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+export default function EffectControl(props) {
+
+    const [enabled, setEnabled] = useState(false)
+
+    const handleClick = (e) => {
+        if (!enabled) setEnabled(true)
+        else setEnabled(false)
+    }
+    return (
+        <div className={enabled ? 'on-effect' : 'off-effect'} onClick={handleClick}>
+            <p>{props.name}</p>
+        </div>
+    )
+}
--- src/components/UI.js
+++ src/components/UI.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import MainOnOff from './MainOnOff'
 import Trigger from './Trigger'
+import EffectControl from './EffectControl'
 
 export default function UI() {
     return (
-        <div className='w-full h-screen flex justify-around content-center'>
-            <div className='w-2/5 h-42 flex flex-wrap justify-around content-center'>
+        <div className='flex w-3/5 m-auto flex-col space-y-12'>
+            <div className='h-42 flex justify-around content-center'>
                 <Trigger />
                 <MainOnOff />
+            </div>
+            <div className='flex justify-around'>
+                <EffectControl name={'reverb'} />
+                <EffectControl name={'filter'} />
             </div>
         </div>
     )
