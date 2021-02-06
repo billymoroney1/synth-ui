962e331b1c374e63c51b3212a6db396b5021d4b5
billymoroney <YOUR-EMAIL-ADDRESS>
2021-02-06 10:51:02 -0600

    added on/off styling for button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react'
+import { Play } from 'heroicons-react'
 
 export default function Button() {
+    const [enabled, setEnabled] = useState(false)
+
+    const handleClick = (e) => {
+        return (enabled ? setEnabled(false) : setEnabled(true))
+    }
+
     return (
-        <div className="off-btn">
-            <p>on/off</p>
+        <div className={enabled ? "on-btn" : "off-btn"} onClick={handleClick}>
+            <Play className='w-32 h-32' />
         </div>
     )
 }
